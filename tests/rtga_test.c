1743076e460bdeeaed8e5678c2fb2fd5e2180eaf
Ryan Cohen <rcohenprogramming@gmail.com>
2021-10-19 18:49:27 -0400

    Added two grayscale image generators as tests.

--- tests/rtga_test.c
+++ tests/rtga_test.c
@@ -29,6 +29,8 @@ int main(void) {
     // File names
     const char *UC_24B_FILENAME = "color24.tga";
     const char *UC_8B_FILENAME = "color8.tga";
+    const char *UC_24B_GRADIENT_FILENAME = "color24_gradient.tga";
+    const char *UC_8B_GRADIENT_FILENAME = "color8_gradient.tga";
 
     // Set initial header values
     header.id_length = 0;
@@ -83,4 +85,28 @@ int main(void) {
         header.width, header.height, header.image_pixel_depth);
     }
     tga_free(&tga);
+
+    // Write a grayscale image gradient
+    header.width = 255;
+    header.height = 255;
+    if (tga_alloc(&tga, header) != TGA_SUCCESS) {
+        printf("Memory allocation error on line %u\n", __LINE__);
+    }
+    for (int y = 0; y < header.height; ++y) {
+        const uint8_t gray_value[] = {y};
+        for (int x = 0; x < header.width; ++x) {
+            tga_set_pixel(&tga, x, y, gray_value);
+        }
+    }
+    success = tga_write_file(&tga, UC_8B_GRADIENT_FILENAME);
+    if (success == TGA_FILE_OPEN_ERROR) {
+        printf("File opening error\n");
+    } else if (success == TGA_FILE_WRITE_ERROR) {
+        printf("File write error\n");
+    } else {
+        printf("Wrote %s with Width{%u},Height{%u},Depth{%u}\n", UC_8B_GRADIENT_FILENAME, 
+        header.width, header.height, header.image_pixel_depth);
+    }
+    tga_free(&tga);
+
 }
